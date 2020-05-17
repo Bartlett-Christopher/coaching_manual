987f8cb6dbb44e91333ba530044405f2c187ce0a
Chris Bartlett <chris.bartlett@therealbuzzgroup.com>
2020-05-17 23:03:35 +0100

    RESTful API post method - gather price information form external API

--- register/api/views/user.py
+++ register/api/views/user.py
@@ -5,6 +5,8 @@
 
 .. moduleauthor:: Chris Bartlett
 """
+import requests
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -14,6 +16,9 @@ from register.api.serializers import UserSerializer
 
 class UserAPIView(APIView):
     """User model API handler."""
+    price_api_url = \
+        'https://us-central1-development-1300.cloudfunctions.net/' \
+        'BETechnicalTest'
 
     def post(self, request, *args, **kwargs):
         """Create a new User."""
@@ -26,10 +31,20 @@ class UserAPIView(APIView):
             )
 
         # call external API
-        price_info = {}
+        price_response = requests.get(
+            self.price_api_url,
+            params={'int_country_code': serializer.data['country']},
+        )
 
-        serializer.save(price_info=price_info)
+        if price_response.status_code != 200:
+            return Response(
+                price_response.json(),
+                status=status.HTTP_502_BAD_GATEWAY
+            )
 
-        response_data = {'price_info': price_info}
+        price_info = price_response.json()
+        serializer.save(price_info=price_info['data'])
+
+        response_data = {'price_info': price_info['data']}
         response_data.update(serializer.data)
         return Response(response_data)
